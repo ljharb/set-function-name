9921c2b64c571255084aefaa02660314609e8ea0
Jordan Harband <ljharb@gmail.com>
2023-09-12 15:03:22 -0700

    [Refactor] use `define-data-property` and `has-property-descriptors`

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 'use strict';
 
+var define = require('define-data-property');
+var hasDescriptors = require('has-property-descriptors')();
+
 var $TypeError = TypeError;
 
 module.exports = function setFunctionName(fn, name) {
 	if (typeof fn !== 'function') {
 		throw new $TypeError('`fn` is not a function');
 	}
-	return Object.defineProperty(fn, 'name', { value: name });
+	if (hasDescriptors) {
+		define(fn, 'name', name, true, true);
+	} else {
+		define(fn, 'name', name);
+	}
+	return fn;
 };
